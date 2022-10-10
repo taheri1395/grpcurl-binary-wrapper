723fe943e91375c7c0731985fbc599cc47ef166c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-10 18:39:53 +0000

    Adding tarfile member sanitization to extractall()

--- grpcurl_binary_wrapper/__init__.py
+++ grpcurl_binary_wrapper/__init__.py
@@ -39,7 +39,26 @@ def download(version=GRPCURL_VERSION):
     urllib.request.urlretrieve(download_url, target_file)
 
     with tarfile.open(target_file) as grpcurl_zip_archive:
-        grpcurl_zip_archive.extractall(extract_directory)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(grpcurl_zip_archive, extract_directory)
 
     executable_path = f"{extract_directory}/grpcurl"
     executable_stat = os.stat(executable_path)
